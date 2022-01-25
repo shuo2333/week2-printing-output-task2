3eed511e1bc25c22c2bb347eda8065c52dcab279
shuo2333 <97558014+shuo2333@users.noreply.github.com>
2022-01-25 02:58:34 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+//Declare the variable first_name and assign it with the value of your first name;
+let first_name = 'Shuo';
+//Declare the variable last_name and assign it with the value of your last name;
+let last_name = 'Hao';
+//Declare the variable email and assign it with the value of your email;
+let email = 'hao00025@algonquinlive.com';
+//Declare the variable output;
+let output = '';
+
+//output += 'My name is ' + (first_name) + ' ' + (last_name) + '. You can contact me at ' + (email) + '.';
+output += "My name is " + (first_name) + " " + (last_name) + ". You can contact me at " + (email) + ".";
+alert(output);

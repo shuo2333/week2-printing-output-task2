3bc9d57ec2003f26d98d4207f9092ccd9b18352f
shuo2333 <97558014+shuo2333@users.noreply.github.com>
2022-01-25 02:58:11 -0500

    Delete index.js

--- js/index.js
+++
@@ -1,9 +0,0 @@
-let first_name = 'Shuo';
-let last_name = 'Hao';
-let email = 'hao00025@algonquinlive.com';
-
-let output = '';
-
-output += "My name is " + (first_name) + " " + (last_name) + ". You can contact me at " + (email) + ".";
-
-alert(output);
